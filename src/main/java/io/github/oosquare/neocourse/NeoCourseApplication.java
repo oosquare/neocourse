f61b8df6c009d403a1dfebc46b43f4bd68cc67e4
oosquare <oosquare@outlook.com>
2024-10-27 22:04:16 +0800

    init: initialize repository

---
+++ src/main/java/io/github/oosquare/neocourse/NeoCourseApplication.java
@@ -0,0 +1,13 @@
+package io.github.oosquare.neocourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/oosquare/neocourse/NeoCourseApplicationTests.java
@@ -0,0 +1,12 @@
+package io.github.oosquare.neocourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoCourseApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
