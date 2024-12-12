934e92d82fd0310472e4c8e57130400b18f953c7
oosquare <oosquare@outlook.com>
2024-12-12 22:54:10 +0800

    feat(ui): add upgrading user to administrator

--- src/main/java/io/github/oosquare/neocourse/ui/view/user/UserView.java
+++ src/main/java/io/github/oosquare/neocourse/ui/view/user/UserView.java
@@ -2,7 +2,13 @@ package io.github.oosquare.neocourse.ui.view.user;
 
 import jakarta.annotation.security.PermitAll;
 
+import java.util.Optional;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.confirmdialog.ConfirmDialog;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.BeforeEvent;
@@ -11,8 +17,11 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import lombok.NonNull;
 
+import io.github.oosquare.neocourse.application.command.user.UpgradeToAdministratorCommand;
+import io.github.oosquare.neocourse.application.command.user.UserCommandService;
 import io.github.oosquare.neocourse.application.query.account.AccountQueryService;
 import io.github.oosquare.neocourse.application.security.CurrentAccountAwareSupport;
+import io.github.oosquare.neocourse.domain.account.model.AccountRoleKind;
 import io.github.oosquare.neocourse.ui.layout.MainLayout;
 import io.github.oosquare.neocourse.utility.id.Id;
 
@@ -24,6 +33,7 @@ public class UserView extends VerticalLayout
 
     public static final String CURRENT_ACCOUNT_USER_PATH = "self";
 
+    private final @NonNull UserCommandService userCommandService;
     private final @NonNull AccountQueryService accountQueryService;
     private String accountId;
 
@@ -31,7 +41,11 @@ public class UserView extends VerticalLayout
     private final @NonNull TextField displayedUsernameField;
     private final @NonNull TextField rolesField;
 
-    public UserView(@NonNull AccountQueryService accountQueryService) {
+    public UserView(
+        @NonNull UserCommandService userCommandService,
+        @NonNull AccountQueryService accountQueryService
+    ) {
+        this.userCommandService = userCommandService;
         this.accountQueryService = accountQueryService;
 
         var title = new H3("User");
@@ -53,10 +67,13 @@ public class UserView extends VerticalLayout
         summaryLayout.setSpacing(false);
         summaryLayout.setPadding(false);
 
-        this.add(title, summaryLayout);
+
+        var buttonLayout = this.createButtonLayout();
+
+        this.add(title, summaryLayout, buttonLayout);
         this.setSizeFull();
     }
-    
+
     @Override
     public void setParameter(BeforeEvent event, String mayBeAccountId) {
         if (CURRENT_ACCOUNT_USER_PATH.equals(mayBeAccountId)) {
@@ -65,6 +82,48 @@ public class UserView extends VerticalLayout
             this.accountId = mayBeAccountId;
         }
         this.updateView();
+    }
+
+    private HorizontalLayout createButtonLayout() {
+        var layout = new HorizontalLayout();
+
+        if (this.getCurrentAccount().hasRole(AccountRoleKind.ADMINISTRATOR)) {
+            layout.add(this.createUpgradeToAdministratorButton());
+        }
+
+        var returnButton = new Button("Return");
+        returnButton.addClickListener(event -> this.navigateToUserListView());
+        layout.add(returnButton);
+
+        return layout;
+    }
+
+    private Button createUpgradeToAdministratorButton() {
+        var dialog = new ConfirmDialog();
+        dialog.setHeader("Upgrade To Administrator");
+        dialog.setText("Do you want to make this user an administrator?");
+        dialog.setCancelable(true);
+        dialog.setRejectable(false);
+        dialog.setConfirmText("Upgrade");
+        dialog.addConfirmListener(event -> this.upgradeAdministrator());
+
+        return new Button("Upgrade To Administrator", event -> {
+            dialog.open();
+        });
+    }
+
+    private void upgradeAdministrator() {
+        var command = UpgradeToAdministratorCommand.builder()
+            .accountId(Id.of(this.accountId))
+            .build();
+        var account = this.getCurrentAccount();
+        this.userCommandService.upgradeToAdministrator(command, account);
+
+        this.updateView();
+    }
+
+    private void navigateToUserListView() {
+        Optional.ofNullable(UI.getCurrent()).ifPresent(ui -> ui.navigate(UserListView.class));
     }
 
     private void updateView() {
